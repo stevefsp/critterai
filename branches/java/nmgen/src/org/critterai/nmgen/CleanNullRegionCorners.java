80513c71b286b8d3eca871857a8dc22ede8c324e
stevefsp <stevefsp@67e97ef4-0a9b-11df-b317-0f3410dc57f6>
2010-08-05 22:40:05 +0000

    NMGen study branch.

---
+++ branches/java/nmgen/src/org/critterai/nmgen/CleanNullRegionCorners.java
@@ -0,0 +1,226 @@
+/*
+ * Copyright (c) 2010 Stephen A. Pratt
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.critterai.nmgen;
+
+/**
+ * Searches for region spans that are adjacent to null region corners and
+ * ensures they are in the most appropriate region.
+ * <p>This algorithm reduces the likelihood of a region flowing slightly around a null region
+ * corner in a manner that results in a contour that is a self-intersecting polygon.</p>
+ * <p>This is an experimental algorithm.  It has only been validated in a limited number of test scenarios.</p>
+ * <p>Example: Before the algorithm is applied.</p> 
+ * <p><a href="http://www.critterai.org/sites/default/files/study/nmgen/ohfg_08_cornerwrapbefore.png"  target="_parent">
+ * <img alt="" src="http://www.critterai.org/sites/default/files/study/nmgen/ohfg_08_cornerwrapbefore.jpg" style="width: 620px; height: 353px; " />
+ * </a></p> 
+ * <p>Example: After the algorithm is applied.</p> 
+ * <p><a href="http://www.critterai.org/sites/default/files/study/nmgen/ohfg_09_cornerwrapafter.png"  target="_parent">
+ * <img alt="" src="http://www.critterai.org/sites/default/files/study/nmgen/ohfg_09_cornerwrapafter.jpg" style="width: 620px; height: 353px; " />
+ * </a></p> 
+ * @see <a href="http://www.critterai.org/nmgen_regiongen" target="_parent">Region Generation</a>
+ * 
+ */
+public final class CleanNullRegionCorners
+    implements IOpenHeightFieldAlgorithm
+{
+
+    /*
+     * Design note: 
+     * 
+     * I investigated several simpler algorithms that did not actually 
+     * care about corners or whether the region flows around the corner.  
+     * For example, one looked at every span connected to the null region
+     * and re-assigned them to the region they have the most connections to.
+     * But while these simpler algorithms fixed the contour issue, they resulted in
+     * a significant, unnecessary increase in thin triangles in the vicinity of corners.
+     * 
+     * Recast Reference: None
+     * 
+     * Doc State: Complete.
+     * Standards Check: Complete
+     */
+    
+    /**
+     * {@inheritDoc}
+     * <p>Behavior is undefined if the height field does not contain 
+     * valid region information.</p>
+     */
+    @Override
+    public void apply(OpenHeightfield field) 
+    {
+        
+        /*
+         * Scan for regions that flow around the corner of a null region.
+         * and determine if the spans that flow around the corner should be
+         * assigned to another region.
+         */
+        
+        IHeightfieldIterator<OpenHeightSpan> iter = field.dataIterator();
+
+        /*
+         * Contains the region IDs of the eight neighbors of the current span.
+         * 
+         * Reference: Neighbor searches and nomenclature.
+         * http://www.critterai.org/?q=nmgen_hfintro#nsearch
+         * 
+         * The first four values are the axis neighbors using the standard
+         * 0-3 direction values.  For example, the axis neighbor at direction
+         * 2 is stored at index 2.
+         * 
+         * The next four values are the diagonal neighbors clockwise to the axis
+         * neighbors.  For example, the neighbor clockwise to axis neighbor
+         * 2 is stored at index 6 (2+4).
+         * 
+         * The lack of a neighbor at a position is marked as a null region.
+         */
+        final int[] nRegions = new int[8];
+        
+        // Loop through all spans.
+        while (iter.hasNext())
+        {
+            /*
+             * Note: The content of the neighbor region array is properly re-populated
+             * during this process.  So there is no need to re-initialize its content
+             * at the beginning of each loop.
+             */
+            
+            final OpenHeightSpan span = iter.next();
+            
+            if (span.regionID() == OpenHeightfield.NULL_REGION_ID)
+                // Don't care about null regions spans.
+                continue;
+            
+            OpenHeightSpan nSpan;
+            boolean onBorder = false;
+            
+            // Search through this span's axis neighbors, gathering regions IDs and determining
+            // if any are in the null region.
+            for (int dir = 0; dir < 4; dir++)
+            {
+                nSpan = span.getNeighbor(dir);
+                if (nSpan == null
+                        || nSpan.regionID() == OpenHeightfield.NULL_REGION_ID)
+                {
+                    // There is a border in this direction.  Mark is as a null region.
+                    onBorder = true;
+                    nRegions[dir] = OpenHeightfield.NULL_REGION_ID;
+                }
+                else
+                    // There is a standard region span in this direction.  Store its region id.
+                    nRegions[dir] = nSpan.regionID();    
+            }
+            
+            if (!onBorder)
+                // Early exit.  We only care about spans that connect to the null region.
+                continue;
+            
+            // Need to get information for diagonal neighbors.
+            for (int dir = 0; dir < 4; dir++)
+            {
+                final int dirCW = (dir+1)&0x03;  // Rotated clockwise from current direction.
+                nSpan = span.getNeighbor(dir);
+                if (nSpan == null)
+                {
+                    // No span in this direction.  Try to find diagonal neighbor
+                    // for this direction via the other axis neighbor that borders it.
+                    nSpan = span.getNeighbor(dirCW);
+                    if (nSpan != null)
+                        // Get diagonal from this neighbor.
+                        nSpan = nSpan.getNeighbor(dir);
+                }
+                else
+                    // Get diagonal from this neighbor.
+                    nSpan = nSpan.getNeighbor(dirCW);                    
+                if (nSpan == null)
+                    // No detectable diagonal neighbor in this direction.
+                    nRegions[dir+4] = OpenHeightfield.NULL_REGION_ID;
+                else
+                    // Get diagonal from this neighbor.
+                    nRegions[dir+4] = nSpan.regionID();
+            }
+            
+            // Detect null region corners wrapped by spans in the same region.
+            int targetRegion = -1;
+            int nullNeighborCount = 0;
+            for (int dir = 0; dir < 4; dir++)
+            {
+                final int dirCW = (dir+1)&0x03;  // The direction clockwise from current direction.
+                final int dirOPP = (dir+2)&0x03;  // The direction that is opposite from the current direction.
+                final int dirCCW = (dir+3)&0x03; // The direction counter clockwise from the current direction.
+                if (nRegions[dir] == OpenHeightfield.NULL_REGION_ID)
+                {
+                    nullNeighborCount++;
+                    // Current span is connected to null region in this direction.
+                    // Check the diagonal spans on each side of the neighbor in this direction.
+                    final int regionCW = nRegions[dir+4];
+                    final int regionCCW = nRegions[dirCCW+4];
+                    /*
+                     * There are two possible corner styles.  Each are detected and handled 
+                     * in a slightly different manner.
+                     *   
+                     * Note: Technically the next checks don't guarantee that this is a true wrap
+                     * since a true wrap requires three spans from the same region to wrap the corner 
+                     * and only two are validated. But the extra check adds unneeded complexity for
+                     * no real benefit.
+                     */
+                    if (regionCW == OpenHeightfield.NULL_REGION_ID && regionCCW == span.regionID())
+                    {
+                        // This corner is wrapped.
+                        if (nRegions[dirCW] != span.regionID() && nRegions[dirCW] == nRegions[dirOPP])
+                        {
+                            // The current span has two axis neighbor connections
+                            // to another region and only one (maximum) to its current region.
+                            // Re-assign to the other region.
+                            targetRegion = nRegions[dirCW];
+                            break;
+                        }
+                    }
+                    else if (regionCW == span.regionID() && regionCCW == OpenHeightfield.NULL_REGION_ID)
+                    {
+                        // This corner is wrapped.
+                        if (nRegions[dirCCW] != span.regionID() && nRegions[dirCCW] == nRegions[dirOPP])
+                        {
+                            // The current span has two axis neighbor connections
+                            // to another region and only one (maximum) to its current region.
+                            // Re-assign to the other region.
+                            targetRegion = nRegions[dirCCW];
+                            break;
+                        }
+                    }
+                }
+            }
+            if (nullNeighborCount != 4 && targetRegion != -1)
+            {
+                /*
+                 * The span is not an island surrounded by the null region
+                 * and it needs re-assignment.
+                 * 
+                 * The island check is needed because this algorithm is not set up to
+                 * handle complete removal of regions.
+                 * 
+                 * Reassign region.
+                 */
+                span.setRegionID(targetRegion);
+            }
+        }
+    }
+
+}
---
+++ branches/java/nmgen/src/org/critterai/nmgen/OpenHeightSpan.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2010 Stephen A. Pratt
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.critterai.nmgen;
+
+/**
+ * Represents the open space above a solid span within the cell column of a heightfield.
+ *  * <p><a href="http://www.critterai.org/sites/default/files/study/nmgen/hf_07_openfield.png" target="_blank">
+ * <img alt="" height="449" src="http://www.critterai.org/sites/default/files/study/nmgen/hf_07_openfield.jpg" width="620" />
+ * </a></p>
+ * <p>Design Note: Structurally this class is so similar with {@link HeightSpan} that this 
+ * class could extend HeightSpan.  But this class is being kept separate for clarity.  
+ * As an independent class it can use the {@link #floor()} and {@link #ceiling()} nomenclature that
+ * makes reading code much easier.</p>
+ * @see <a href="http://www.critterai.org/?q=nmgen_hfintro" target="_parent">Introduction to Heightfields</a>
+ * @see HeightSpan
+ */
+public final class OpenHeightSpan 
+{
+    /*
+     * Doc State: Complete
+     * Standards Check: Complete
+     */
+    
+    /**
+     * Flags used for for temporary flagging.
+     * The value is meaningless outside the operation in which the flags are needed.
+     * Since various operations may use this flag for their own purpose, always reset
+     * the flag prior to use.
+     */
+    public int flags = 0;
+    
+    private int mRegionID = 0;
+    private int mDistanceToRegionCore = 0;
+    private int mDistanceToBorder = 0;
+    
+    private int mFloor;
+    private int mHeight;
+    
+    private OpenHeightSpan mNext = null;
+    private OpenHeightSpan mNeighborConnection0 = null;
+    private OpenHeightSpan mNeighborConnection1 = null;
+    private OpenHeightSpan mNeighborConnection2 = null;
+    private OpenHeightSpan mNeighborConnection3 = null;
+    
+    /**
+     * Constructor
+     * @param floor The base height of the span.
+     * @param height The height of the unobstructed space above the floor. 
+     * {@link Integer#MAX_VALUE} is generally used to indicate no obstructions exist above the floor. 
+     * @throws IllegalArgumentException If the floor is below zero or the height is less than 1.
+     */
+    public OpenHeightSpan(int floor, int height)
+        throws IllegalArgumentException
+    {
+        if (floor < 0)
+            throw new IllegalArgumentException("Floor is less than zero.");
+        if (height < 1)
+            throw new IllegalArgumentException("Height is less than one.");
+        this.mFloor = floor;
+        this.mHeight = height;
+    }
+    
+    /**
+     * The base height of the span.
+     * @return The base height of the span.
+     */
+    public int floor() { return mFloor; }
+    
+    /**
+     * The height of the unobstructed space above the floor. 
+     * <p>{@link Integer#MAX_VALUE} is generally used to indicate no obstructions exist above the floor.</p> 
+     * @return The height of the unobstructed space above the floor. 
+     */
+    public int height() { return mHeight; }
+    
+    /**
+     * The height of the ceiling.
+     * @return The height of the ceiling.
+     */
+    public int ceiling() { return mFloor + mHeight; }
+    
+    /**
+     * The next span higher in the span's heightfield column.
+     * <p>The space between this span's ceiling and the next span's floor is
+     * considered to be obstructed space.</p>
+     * @return The next higher span in the span's heightfield column.  Or null if there
+     * is no heigher span.
+     */
+    public OpenHeightSpan next() { return mNext; }
+    
+    /**
+     * Set the next heigher span in the span's heightfield column.
+     * @param value The new value.  null is an acceptable value.
+     */
+    public void setNext(OpenHeightSpan value) { mNext = value; }
+    
+    /**
+     * The distance this span is from the nearest border of the heightfield it belongs to.
+     * @return The distance this span is from the nearest heightfield border.
+     */
+    public int distanceToBorder() { return mDistanceToBorder; }
+    
+    /**
+     * Set the distance this span is from the nearest border of the heightfield it belongs to.
+     * @param value The new distance.  Auto-clamped at a minimum of zero.
+     */
+    public void setDistanceToBorder(int value)
+    {
+        mDistanceToBorder = Math.max(value, 0);
+    }
+    
+    /**
+     * The distance this span is from the core of the heightfield region it belongs to.
+     * @return The distance this span is from the core of the heightfield region it belongs to.
+     */
+    public int distanceToRegionCore() { return mDistanceToRegionCore; }
+    
+    /**
+     * Set the distance this span is from the core of the heightfield region it belongs to.
+     * @param value The new distance.  Auto-clamped at a minimum of zero.
+     */
+    public void setDistanceToRegionCore(int value)
+    {
+        mDistanceToRegionCore = Math.max(value, 0);
+    }
+    
+    /**
+     * The heightfield region this span belongs to.
+     * @return The heightfield region this span belongs to.
+     */
+    public int regionID() { return mRegionID; }
+    
+    /**
+     * The heightfield region this span belongs to.
+     * @param value The new value.
+     */
+    public void setRegionID(int value) { mRegionID = value; }
+    
+    /**
+     * Gets a reference to the span that is considered an axis-neighbor to this span for
+     * the specified direction.  Uses the standard direction indices (0 through 3) where
+     * Zero is the neighbor offset at (-1, 0) and the search proceeds clockwise.
+     * @param direction  The direction to search.
+     * @return A reference to the axis-neighbor in the specified direction.  Or null if there
+     * is no neighbor in the direction or the direction index is invalid.
+     * @see <a href="http://www.critterai.org/?q=nmgen_hfintro#nsearch" target="_parent">Neighbor Searches</a>
+     */
+    public OpenHeightSpan getNeighbor(int direction)
+    {
+        switch (direction)
+        {
+        case 0: return mNeighborConnection0;
+        case 1: return mNeighborConnection1;
+        case 2: return mNeighborConnection2;
+        case 3: return mNeighborConnection3;
+        default: return null;
+        }
+    }
+    
+    /**
+     * Sets the specified span at the neighbor of the current span.
+     * <p>Uses the standard direction indices (0 through 3) where
+     * Zero is the neighbor offset at (-1, 0) and the search proceeds clockwise.</p>
+     * @param direction The direction of the neighbor.
+     * @param neighbor The neighbor of this span.
+     * @see <a href="http://www.critterai.org/?q=nmgen_hfintro#nsearch" target="_parent">Neighbor Searches</a>
+     */
+    public void setNeighbor(int direction, OpenHeightSpan neighbor)
+    {
+        switch (direction)
+        {
+        case 0: mNeighborConnection0 = neighbor; break;
+        case 1: mNeighborConnection1 = neighbor; break;
+        case 2: mNeighborConnection2 = neighbor; break;
+        case 3: mNeighborConnection3 = neighbor; break;
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() 
+    {
+        return "floor: " + mFloor + ", ceiling: " + mHeight + ", Region: " + mRegionID;
+    }
+    
+}
