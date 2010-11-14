c607531f91665c1956067d1a5a41e4b5738aa71b
stevefsp <stevefsp@67e97ef4-0a9b-11df-b317-0f3410dc57f6>
2010-11-14 23:44:10 +0000

    New Methods and Overloads (cai-util-u3d)

--- trunk/u3d/util/math/Vector2Util.cs
+++ trunk/u3d/util/math/Vector2Util.cs
@@ -25,13 +25,14 @@ using UnityEngine;
 namespace org.critterai.math
 {
     /// <summary>
-    /// Contains various static operations applicable to 2D vectors which are not provided
+    /// Provides various 2D vector utility methods which are not provided
     /// by the Unity3D vector class.
     /// </summary>
     /// <remarks>
-    /// <para>This class is optimized for speed.  To support this priority, no argument validation is
-    /// performed.  E.g. No null checks, no divide by zero checks, etc.</para>
-    /// <para>Static operations are thread safe.</para>
+    /// <p>This class is optimized for speed.  To support this priority, no 
+    /// argument validation is performed.  E.g. No null checks, no divide 
+    /// by zero checks, etc.</p>
+    /// <p>Static methods are thread safe.</p>
     /// </remarks>
     public static class Vector2Util
     {
@@ -40,7 +41,8 @@ namespace org.critterai.math
 
         /// <summary>
         /// Returns the dot product of the provided vectors.
-        /// See: <a href="http://en.wikipedia.org/wiki/Dot_product" target="_blank">
+        /// See: 
+        /// <a href="http://en.wikipedia.org/wiki/Dot_product" target="_blank">
         /// Wikipedia- Dot Product</a>
         /// </summary>
         /// <remarks></remarks>
@@ -55,15 +57,20 @@ namespace org.critterai.math
         }
 
         /// <summary>
-        /// Derives the normalized direction vector for the vector pointing from point A (ax, ay) to
-        /// point B (bx, by).
-        /// <para>WARNING: This is a costly operation.</para>
+        /// Derives the normalized direction vector for the vector pointing 
+        /// from point A (ax, ay) to point B (bx, by).
+        /// <p>WARNING: This is a costly method.</p>
         /// </summary>
-        /// <param name="ax">The x-value for the starting point A (ax, ay).</param>
-        /// <param name="ay">The y-value for the starting point A (ax, ay).</param>
-        /// <param name="bx">The x-value for the end point B (bx, by).</param>
-        /// <param name="by">The y-value for the end point B (bx, by).</param>
-        /// <returns>The normalized direction vector for the vector pointing from point A to B.</returns>
+        /// <param name="ax">The x-value for the starting point A (ax, ay).
+        /// </param>
+        /// <param name="ay">The y-value for the starting point A (ax, ay).
+        /// </param>
+        /// <param name="bx">The x-value for the end point B (bx, by).
+        /// </param>
+        /// <param name="by">The y-value for the end point B (bx, by).
+        /// </param>
+        /// <returns>The normalized direction vector for the vector pointing 
+        /// from point A to B.</returns>
         public static Vector2 GetDirectionAB(float ax, float ay
                 , float bx, float by)
         {
@@ -71,7 +78,8 @@ namespace org.critterai.math
             Vector2 result = new Vector2(bx - ax, by - ay);
             
             // Normalize.
-            float length = (float)Math.Sqrt((result.x * result.x) + (result.y * result.y));
+            float length = (float)
+                Math.Sqrt((result.x * result.x) + (result.y * result.y));
             if (length <= EPSILON_STD) 
                 length = 1;
             
@@ -87,60 +95,72 @@ namespace org.critterai.math
         }
 
         /// <summary>
-        /// Determines whether or not the elements of the provided vectors are equal within
-        /// the specified tolerance.  Each element of the vector is tested separately.
+        /// Determines whether or not the elements of the provided vectors 
+        /// are equal within the specified tolerance.  Each element of the 
+        /// vector is tested separately.
         /// </summary>
         /// <param name="ux">The x-value of the vector (ux, uy).</param>
         /// <param name="uy">The y-value of the vector (ux, uy).</param>
         /// <param name="vx">The x-value of the vector (vx, vy).</param>
         /// <param name="vy">The y-value of the vector (vx, vy).</param>
         /// <param name="tolerance">The tolerance for the test.</param>
-        /// <returns>TRUE if the the associated elements of each vector are within the specified tolerance
-        /// of each other.  Otherwise FALSE.</returns>
-        public static Boolean SloppyEquals(float ux, float uy, float vx, float vy, float tolerance)
+        /// <returns>TRUE if the the associated elements of each vector are 
+        /// within the specified tolerance of each other.  Otherwise FALSE.
+        /// </returns>
+        public static bool SloppyEquals(float ux, float uy
+            , float vx, float vy
+            , float tolerance)
         {
             tolerance = Math.Max(0, tolerance);
-            return !(Math.Abs(ux - vx) > tolerance || Math.Abs(uy - vy) > tolerance);
+            return !(Math.Abs(ux - vx) > tolerance 
+                || Math.Abs(uy - vy) > tolerance);
         }
 
         /// <summary>
-        /// Determines whether or not the elements of the provided vectors are equal within
-        /// the specified tolerance.  Each element of the vector is tested separately.
+        /// Determines whether or not the elements of the provided vectors 
+        /// are equal within the specified tolerance.  Each element of the 
+        /// vector is tested separately.
         /// </summary>
         /// <param name="u">Vector v</param>
         /// <param name="v">Vector u</param>
         /// <param name="tolerance">The tolerance for the test. </param>
-        /// <returns>TRUE if the the associated elements of each vector are within the specified tolerance
-        /// of each other.  Otherwise FALSE.</returns>
-        public static Boolean SloppyEquals(Vector2 u, Vector2 v, float tolerance)
+        /// <returns>TRUE if the the associated elements of each vector are
+        /// within the specified tolerance of each other.  Otherwise FALSE.
+        /// </returns>
+        public static bool SloppyEquals(Vector2 u, Vector2 v, float tolerance)
         {
             return SloppyEquals(u.x, u.y, v.x, v.y, tolerance);
         }
 
         /// <summary>
-        /// Determines whether or not the elements of the provided vectors are equal within
-        /// the specified tolerance.  Each element of the vector is tested separately.
+        /// Determines whether or not the elements of the provided vectors 
+        /// are equal within the specified tolerance.  Each element of the 
+        /// vector is tested separately.
         /// </summary>
         /// <param name="u">Vector v</param>
         /// <param name="vx">The x-value of the vector (vx, vy).</param>
         /// <param name="vy">The y-value of the vector (vx, vy).</param>
         /// <param name="tolerance">The tolerance for the test.</param>
-        /// <returns>TRUE if the the associated elements of each vector are within the specified tolerance
-        /// of each other.  Otherwise FALSE.</returns>
-        public static Boolean SloppyEquals(Vector2 u, float vx, float vy, float tolerance)
+        /// <returns>TRUE if the the associated elements of each vector are 
+        /// within the specified tolerance of each other.  Otherwise FALSE.
+        /// </returns>
+        public static bool SloppyEquals(Vector2 u, float vx, float vy, float tolerance)
         {
             return SloppyEquals(u.x, u.y, vx, vy, tolerance);
         }
 
         /// <summary>
-        /// Returns the square of the distance between the two provided points. (distance * distance)
+        /// Returns the square of the distance between the two provided 
+        /// points. (distance * distance)
         /// </summary>
         /// <param name="ax">The x-value of the point (ax, ay).</param>
         /// <param name="ay">The y-value of the point (ax, ay).</param>
         /// <param name="bx">The x-value of the point (bx, by).</param>
         /// <param name="by">The y-value of the point (bx, by).</param>
-        /// <returns>The square of the distance between the two provided points.</returns>
-        public static float GetDistanceSq(float ax, float ay, float bx, float by)
+        /// <returns>The square of the distance between the two provided 
+        /// points.</returns>
+        public static float GetDistanceSq(float ax, float ay
+            , float bx, float by)
         {
             float dx = ax - bx;
             float dy = ay - by;
@@ -148,8 +168,9 @@ namespace org.critterai.math
         }
 
         /// <summary>
-        /// Normalizes the provided vector such that its length is equal to one.
-        /// <para>WARNING: This is a costly operation.</para>
+        /// Normalizes the provided vector such that its length is equal to 
+        /// one.
+        /// <p>WARNING: This is a costly method.</p>
         /// </summary>
         /// <param name="x">The x-value of the vector (x, y).</param>
         /// <param name="y">The y-value of the vector (x, y).</param>
@@ -173,7 +194,7 @@ namespace org.critterai.math
 
         /// <summary>
         /// Scales the vector to the provided length.
-        /// <para>WARNING: This is a costly operation.</para>
+        /// <p>WARNING: This is a costly method.</p>
         /// </summary>
         /// <param name="x">The x-value of the vector (x, y).</param>
         /// <param name="y">The y-value of the vector (x, y).</param>
@@ -188,9 +209,9 @@ namespace org.critterai.math
         }
 
         /// <summary>
-        /// Scales the vector to the provided length. Returns a new vector. The
-        /// original vector is not mutated.
-        /// <para>WARNING: This is a costly operation.</para>
+        /// Scales the vector to the provided length. Returns a new vector. 
+        /// The original vector is not mutated.
+        /// <p>WARNING: This is a costly method.</p>
         /// </summary>
         /// <param name="v">The vector to scale.</param>
         /// <param name="length">The length to scale the vector to.</param>
@@ -198,6 +219,57 @@ namespace org.critterai.math
         public static Vector2 ScaleTo(Vector2 v, float length) 
         {
             return ScaleTo(v.x, v.y, length);
+        }
+
+        /// <summary>
+        /// Truncates the length of the vector to the provided value.
+        /// </summary>
+        /// <remarks>
+        /// <p>If the vector's length is longer than the provided value the 
+        ///  length of the vector is scaled back to the provided maximum 
+        ///  length.</p>
+        ///  <p>If the vector's length is shorter than the provided value, 
+        ///  the  vector is not changed.</p>
+        ///  <p>WARNING: This is a potentially costly method.</p>
+        /// </remarks>
+        /// <param name="x">The x-value of the vector (x, y).</param>
+        /// <param name="y">The y-value of the vector (x, y).</param>
+        /// <param name="maxLength">The maximum allowed length of the 
+        /// resulting vector.</param>
+        /// <returns>A vector with a length at or below the maximum
+        /// length.</returns>
+        public static Vector2 TruncateLength(float x, float y, float maxLength) 
+        {
+            if (maxLength == 0 || (x < float.Epsilon && y < float.Epsilon))
+            { 
+                return new Vector2(0, 0);
+            }
+            float csq = x * x + y * y;
+            if (csq <= maxLength * maxLength)
+                return new Vector2(x, y);
+            float factor = (float)(maxLength / Math.Sqrt(csq));
+            return new Vector2(x * factor, y * factor);
+        }
+
+        /// <summary>
+        /// Truncates the length of the vector to the provided value.
+        /// </summary>
+        /// <remarks>
+        /// <p>If the vector's length is longer than the provided value 
+        /// the length of the vector is scaled back to the provided
+        /// maximum length.</p>
+        /// <p>If the vector's length is shorter than the provided value, 
+        /// the vector is not changed.</p>
+        /// <p>WARNING: This is a potentially costly method.</p>
+        /// </remarks>
+        /// <param name="v">The vector to scale.</param>
+        /// <param name="maxLength">The maximum allowed length of the 
+        /// resulting vector.</param>
+        /// <returns>A vector with a length at or below the maximum
+        /// length.</returns>
+        public static Vector2 TruncateLength(Vector2 v, float maxLength) 
+        {
+            return TruncateLength(v.x, v.y, maxLength);
         }
     }
 }
