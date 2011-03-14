0afa00d454442d6520558b1223b41120f6457603
stevefsp <stevefsp@67e97ef4-0a9b-11df-b317-0f3410dc57f6>
2011-03-14 17:56:42 +0000

    cai-util: Namespace simplification.

---
+++ trunk/util/u3d/ColorUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace org.critterai
+{
+    public static class ColorUtil
+    {
+        // Returns 1 if the bit at position b in a is 1
+        // Otherwise returns 0.
+        private static int bit(int a, int b)
+        {
+            return (a & (1 << b)) >> b;
+        }
+
+        public static Color IntToColor(int i, float a)
+        {
+            // r, g, and b are constrained to between 1 and 4 inclusive.
+            float factor = 63 / 255;  // Approximately 0.25.
+	        float r = bit(i, 1) + bit(i, 3) * 2 + 1;
+	        float g = bit(i, 2) + bit(i, 4) * 2 + 1;
+	        float b = bit(i, 0) + bit(i, 5) * 2 + 1;
+            return new Color(r * factor, g * factor, b * factor, a);
+        }
+    }
+}
