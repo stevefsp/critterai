2a008671c341178f5fb2b2be8d29bea5b0fdfbef
stevefsp <stevefsp@67e97ef4-0a9b-11df-b317-0f3410dc57f6>
2011-03-28 23:18:58 +0000

    cai-nav-rcn: Add primary navmesh and query interop functionality.

--- nav/rcn/src/DetourNavMeshQueryEx.cpp
+++ nav/rcn/src/DetourNavMeshQueryEx.cpp
@@ -29,6 +29,20 @@ extern "C"
         dtFreeNavMeshQuery(*pNavQuery);
     }
 
+	EXPORT_API dtStatus dtqGetPolyWallSegments(dtNavMeshQuery* query 
+        , const dtPolyRef ref
+        , const dtQueryFilter* filter
+	    , float* segments
+        , int* segmentCount
+        , const int maxSegments)
+    {
+        return query->getPolyWallSegments(ref
+            , filter
+            , segments
+            , segmentCount
+            , maxSegments);
+    }
+
 	EXPORT_API dtStatus dtqFindNearestPoly(dtNavMeshQuery* query
         , const float* center
         , const float* extents
@@ -42,4 +56,246 @@ extern "C"
             , nearestRef
             , nearestPt);
     }
+
+    EXPORT_API dtStatus dtqQueryPolygons(dtNavMeshQuery* query 
+        , const float* center
+        , const float* extents
+		, const dtQueryFilter* filter
+		, dtPolyRef* polyIds
+        , int* polyCount
+        , const int maxPolys)
+    {
+        return query->queryPolygons(center
+            , extents
+            , filter
+            , polyIds
+            , polyCount
+            , maxPolys);
+    }
+
+	EXPORT_API dtStatus dtqFindPolysAroundCircle(dtNavMeshQuery* query 
+        , dtPolyRef startRef
+        , const float* centerPos
+        , const float radius
+	    , const dtQueryFilter* filter
+	    , dtPolyRef* resultPolyRefs
+        , dtPolyRef* resultParentRefs
+        , float* resultCosts
+	    , int* resultCount
+        , const int maxResult)
+    {
+        return query->findPolysAroundCircle(startRef
+            , centerPos
+            , radius
+            , filter
+            , resultPolyRefs
+            , resultParentRefs
+            , resultCosts
+            , resultCount
+            , maxResult);
+    }
+
+	EXPORT_API dtStatus dtqFindPolysAroundShape(dtNavMeshQuery* query 
+        , dtPolyRef startRef
+        , const float* verts
+        , const int nverts
+	    , const dtQueryFilter* filter
+	    , dtPolyRef* resultRef
+        , dtPolyRef* resultParent
+        , float* resultCost
+	    , int* resultCount
+        , const int maxResult)
+    {
+        return query->findPolysAroundShape(startRef
+            , verts
+            , nverts
+            , filter
+            , resultRef
+            , resultParent
+            , resultCost
+            , resultCount
+            , maxResult);
+    }
+
+	EXPORT_API dtStatus dtqFindLocalNeighbourhood(dtNavMeshQuery* query 
+        , dtPolyRef startRef
+        , const float* centerPos
+        , const float radius
+	    , const dtQueryFilter* filter
+	    , dtPolyRef* resultRef
+        , dtPolyRef* resultParent
+	    , int* resultCount
+        , const int maxResult)
+    {
+        return query->findLocalNeighbourhood(startRef
+            , centerPos
+            , radius
+            , filter
+            , resultRef
+            , resultParent
+            , resultCount
+            , maxResult);
+    }
+
+    EXPORT_API dtStatus dtqClosestPointOnPoly(dtNavMeshQuery* query 
+        , dtPolyRef ref
+        , const float* pos
+        , float* closest)
+    {
+        return query->closestPointOnPoly(ref, pos, closest);
+    }
+
+	EXPORT_API dtStatus dtqClosestPointOnPolyBoundary(dtNavMeshQuery* query 
+        , dtPolyRef ref
+        , const float* pos
+        , float* closest)
+    {
+        return query->closestPointOnPolyBoundary(ref, pos, closest);
+    }
+
+    EXPORT_API dtStatus dtqGetPolyHeight(dtNavMeshQuery* query
+        , dtPolyRef ref
+        , const float* pos
+        , float* height)
+    {
+        return query->getPolyHeight(ref, pos, height);
+    }
+
+	EXPORT_API dtStatus dtqFindDistanceToWall(dtNavMeshQuery* query
+        , dtPolyRef startRef
+        , const float* centerPos
+        , const float maxRadius
+	    , const dtQueryFilter* filter
+	    , float* hitDist
+        , float* hitPos
+        , float* hitNormal)
+    {
+        return query->findDistanceToWall(startRef
+            , centerPos
+            , maxRadius
+            , filter
+            , hitDist
+            , hitPos
+            , hitNormal);
+    }
+
+    EXPORT_API dtStatus dtqFindPath(dtNavMeshQuery* query 
+        , dtPolyRef startRef
+        , dtPolyRef endRef
+		, const float* startPos
+        , const float* endPos
+		, const dtQueryFilter* filter
+		, dtPolyRef* path
+        , int* pathCount
+        , const int maxPath)
+    {
+        return query->findPath(startRef
+            , endRef
+            , startPos
+            , endPos
+            , filter
+            , path
+            , pathCount
+            , maxPath);
+    }
+
+	EXPORT_API bool dtqIsInClosedList(dtNavMeshQuery* query
+        , dtPolyRef ref)
+    {
+        return query->isInClosedList(ref);
+    }
+
+	EXPORT_API dtStatus dtqRaycast(dtNavMeshQuery* query
+        , dtPolyRef startRef
+        , const float* startPos
+        , const float* endPos
+	    , const dtQueryFilter* filter
+	    , float* t
+        , float* hitNormal
+        , dtPolyRef* path
+        , int* pathCount
+        , const int maxPath)
+    {
+        return query->raycast(startRef
+            , startPos
+            , endPos
+            , filter
+            , t
+            , hitNormal
+            , path
+            , pathCount
+            , maxPath);
+    }
+
+    EXPORT_API dtStatus dtqFindStraightPath(dtNavMeshQuery* query
+        , const float* startPos
+        , const float* endPos
+		, const dtPolyRef* path
+        , const int pathSize
+	    , float* straightPath
+        , unsigned char* straightPathFlags
+        , dtPolyRef* straightPathRefs
+	    , int* straightPathCount
+        , const int maxStraightPath)
+    {
+        return query->findStraightPath(startPos
+            , endPos
+            , path
+            , pathSize
+            , straightPath
+            , straightPathFlags
+            , straightPathRefs
+            , straightPathCount
+            , maxStraightPath);
+    }
+
+    EXPORT_API dtStatus dtqMoveAlongSurface(dtNavMeshQuery* query
+        , dtPolyRef startRef
+        , const float* startPos
+        , const float* endPos
+	    , const dtQueryFilter* filter
+	    , float* resultPos
+        , dtPolyRef* visited
+        , int* visitedCount
+        , const int maxVisitedSize)
+    {
+        return query->moveAlongSurface(startRef
+            , startPos
+            , endPos
+            , filter
+            , resultPos
+            , visited
+            , visitedCount
+            , maxVisitedSize);
+    }
+
+	EXPORT_API dtStatus dtqInitSlicedFindPath(dtNavMeshQuery* query
+        , dtPolyRef startRef
+        , dtPolyRef endRef
+        , const float* startPos
+        , const float* endPos
+        , const dtQueryFilter* filter)
+    {
+        return query->initSlicedFindPath(startRef
+            , endRef
+            , startPos
+            , endPos
+            , filter);
+    }
+
+	EXPORT_API dtStatus dtqUpdateSlicedFindPath(dtNavMeshQuery* query
+        , const int maxIter
+        , int* doneIters)
+    {
+        return query->updateSlicedFindPath(maxIter, doneIters);
+    }
+
+	EXPORT_API dtStatus dtqFinalizeSlicedFindPath(dtNavMeshQuery* query
+        , dtPolyRef* path
+        , int* pathCount
+        , const int maxPath)
+    {
+        return query->finalizeSlicedFindPath(path, pathCount, maxPath);
+    }
+
 }
--- nav/rcn/src/DetourNavmeshEx.cpp
+++ nav/rcn/src/DetourNavmeshEx.cpp
@@ -21,12 +21,163 @@
  */
 #include <string>
 #include "DetourNavMeshEx.h"
+#include "DetourCommon.h"
 
 extern "C"
 {
     EXPORT_API void freeDTNavMesh(dtNavMesh** pNavMesh)
     {
         dtFreeNavMesh(*pNavMesh);
+    }
+
+    EXPORT_API void dtnmGetParams(const dtNavMesh* pNavMesh
+        , dtNavMeshParams* params)
+    {
+        if (!pNavMesh)
+            return;
+
+        const dtNavMeshParams* lparams = pNavMesh->getParams();
+
+        params->maxPolys = lparams->maxPolys;
+        params->maxTiles = lparams->maxTiles;
+        params->tileHeight = lparams->tileHeight;
+        params->tileWidth = lparams->tileWidth;
+
+        dtVcopy(params->orig, lparams->orig);
+    }
+
+    EXPORT_API int dtnmGetMaxTiles(const dtNavMesh* pNavMesh)
+    {
+        if (!pNavMesh)
+            return -1;
+
+        return pNavMesh->getMaxTiles();
+    }
+
+    EXPORT_API bool dtnmIsValidPolyRef(const dtNavMesh* pNavMesh
+        , const dtPolyRef polyRef)
+    {
+        if (!pNavMesh)
+            return false;
+
+        return pNavMesh->isValidPolyRef(polyRef);
+    }
+
+    //EXPORT_API dtStatus dtnmGetOffMeshConnectionPolyEndPoints(
+    //    const dtNavMesh* pNavMesh
+    //    , const dtPolyRef prevRef
+    //    , const dtPolyRef polyRef
+    //    , float* startPos
+    //    , float* endPos)
+    //{
+    //    if (!pNavMesh)
+    //        return (DT_FAILURE | DT_INVALID_PARAM);
+
+    //    return pNavMesh->getOffMeshConnectionPolyEndPoints(
+    //        prevRef
+    //        , polyRef
+    //        , startPos
+    //        , endPos);
+    //}
+
+    EXPORT_API dtStatus dtnmGetTileInfo(const dtNavMesh* pNavMesh
+        , const int tileIndex
+        , rcnTileInfo* tileInfo)
+    {
+        if (!pNavMesh || !tileInfo
+                || tileIndex < 0
+                || tileIndex >= pNavMesh->getMaxTiles())
+            return (DT_FAILURE | DT_INVALID_PARAM);
+
+        const dtMeshTile* tile = pNavMesh->getTile(tileIndex);
+        dtMeshHeader* header = tile->header;
+
+        tileInfo->polyCount = header->polyCount;
+        tileInfo->vertCount = header->vertCount;
+        tileInfo->walkableClimb = header->walkableClimb;
+        tileInfo->walkableHeight = header->walkableHeight;
+        tileInfo->walkableRadius = header->walkableRadius;
+        tileInfo->x = header->x;
+        tileInfo->y = header->y;
+        dtVcopy(tileInfo->bmin, header->bmin);
+        dtVcopy(tileInfo->bmax, header->bmax);
+        tileInfo->basePolyRef = pNavMesh->getPolyRefBase(tile);
+
+        return DT_SUCCESS;
+    }
+
+    EXPORT_API dtStatus dtnmGetPolyInfo(const dtNavMesh* pNavMesh
+        , const dtPolyRef polyRef
+        , rcnPolyInfo* polyInfo)
+    {
+        if (!pNavMesh || !polyInfo || !polyRef)
+            return (DT_FAILURE | DT_INVALID_PARAM);
+
+        const dtMeshTile* tile = 0;
+        const dtPoly* poly = 0;
+        dtStatus status = pNavMesh->getTileAndPolyByRef(polyRef
+            , &tile
+            , &poly);
+
+        if (dtStatusFailed(status))
+            return status;
+
+        polyInfo->areaAndtype = poly->areaAndtype;
+        polyInfo->flags = poly->flags;
+        polyInfo->vertCount = poly->vertCount;
+
+        for (int i = 0; i < poly->vertCount; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                float svi = tile->verts[poly->verts[i] * 3 + j];
+
+                polyInfo->verts[i * 3 + j] = 
+                    tile->verts[poly->verts[i] * 3 + j];
+            }
+        }
+
+        return DT_SUCCESS;
+    }
+
+    EXPORT_API dtStatus dtnmGetPolyFlags(const dtNavMesh* pNavMesh
+        , const dtPolyRef polyRef
+        , unsigned short* flags)
+    {
+        if (!pNavMesh || !polyRef)
+            return (DT_FAILURE | DT_INVALID_PARAM);
+
+        return pNavMesh->getPolyFlags(polyRef, flags);
+    }
+
+    EXPORT_API dtStatus dtnmSetPolyFlags(dtNavMesh* pNavMesh
+        , const dtPolyRef polyRef
+        , unsigned short flags)
+    {
+        if (!pNavMesh || !polyRef)
+            return (DT_FAILURE | DT_INVALID_PARAM);
+
+        return pNavMesh->setPolyFlags(polyRef, flags);
+    }
+
+    EXPORT_API dtStatus dtnmGetPolyArea(const dtNavMesh* pNavMesh
+        , const dtPolyRef polyRef
+        , unsigned char* area)
+    {
+        if (!pNavMesh || !polyRef)
+            return (DT_FAILURE | DT_INVALID_PARAM);
+
+        return pNavMesh->getPolyArea(polyRef, area);
+    }
+
+    EXPORT_API dtStatus dtnmSetPolyArea(dtNavMesh* pNavMesh
+        , const dtPolyRef polyRef
+        , unsigned char area)
+    {
+        if (!pNavMesh || !polyRef)
+            return (DT_FAILURE | DT_INVALID_PARAM);
+
+        return pNavMesh->setPolyArea(polyRef, area);
     }
 
 }
