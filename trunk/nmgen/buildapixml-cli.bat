02766a2e632d6c32092fa74b2e10da790966a54f
stevefsp <stevefsp@67e97ef4-0a9b-11df-b317-0f3410dc57f6>
2011-05-09 15:42:56 +0000

    cai-nmgen: Prepare SVN for v0.2.

--- trunk/nmgen/buildapixml-cli.bat
+++
@@ -1,5 +0,0 @@
-ECHO OFF
-REM Must be run from VS command prompt.
-REM Only applies to the core library's .NET documentation.
-
-xdcmake ".\cpp\cli\obj\Release\*.xdc"  /out:".\cpp\bin\Release\cai-nmgen-cli.xml"
