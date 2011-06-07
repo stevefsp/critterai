7423ca87827cb16e724c42c29697c0dc9508fc92
stevefsp <stevefsp@67e97ef4-0a9b-11df-b317-0f3410dc57f6>
2011-06-07 14:46:14 +0000

    cai-util-u3d: v0.3: Add missing source files.

---
+++ util/u3d/GLUtil.cs
@@ -0,0 +1,110 @@
+﻿/*
+ * Copyright (c) 2011 Stephen A. Pratt
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using UnityEngine;
+
+namespace org.critterai
+{
+    /// <summary>
+    /// Provides various utility functions related to GL rendering.
+    /// </summary>
+    public static class GLUtil
+    {
+        private static Material mSimpleMaterial = null;
+
+        /// <summary>
+        /// Provides a shared material suitable for simple drawing operations.
+        /// (Such as debug visualizations.)
+        /// </summary>
+        public static Material SimpleMaterial
+        {
+            get
+            {
+                if (!mSimpleMaterial)
+                {
+                    mSimpleMaterial = new Material(
+                        "Shader \"Lines/Colored Blended\" {"
+                        + "SubShader { Pass { "
+                        + "	BindChannels { Bind \"Color\",color } "
+                        + "	Blend SrcAlpha OneMinusSrcAlpha "
+                        + "	ZWrite Off Cull Off Fog { Mode Off } "
+                        + "} } }");
+                    mSimpleMaterial.hideFlags = HideFlags.HideAndDontSave;
+                    mSimpleMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
+                }
+                return mSimpleMaterial;
+            }
+        }
+
+        /// <summary>
+        /// Draws the provided triangle mesh in a manner suitable for 
+        /// debug visualizations. 
+        /// (Wiremesh with a low alpha transparent surface.)
+        /// </summary>
+        /// <remarks>
+        /// <para>This method uses GL.  So it should usually be called within 
+        /// OnRenderObject().</para>
+        /// </remarks>
+        /// <param name="vertices">The vertices.</param>
+        /// <param name="triangles">The triangle indices.</param>
+        /// <param name="drawColor">The base color to use. (Alpha is ignored.)
+        /// </param>
+        public static void DrawTriMesh(float[] vertices
+            , int[] triangles
+            , Color drawColor)
+        {
+            GLUtil.SimpleMaterial.SetPass(0);
+
+            drawColor.a = 0.25f;
+
+            GL.Begin(GL.TRIANGLES);
+            GL.Color(drawColor);
+            for (int p = 0; p < triangles.Length; p += 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    GL.Vertex3(vertices[triangles[p + i] * 3 + 0]
+                        , vertices[triangles[p + i] * 3 + 1]
+                        , vertices[triangles[p + i] * 3 + 2]);
+                }
+            }
+            GL.End();
+
+            drawColor.a = 0.4f;
+
+            for (int p = 0; p < triangles.Length; p += 3)
+            {
+                GL.Begin(GL.LINES);
+                GL.Color(drawColor);
+                for (int i = 0; i < 3; i++)
+                {
+                    GL.Vertex3(vertices[triangles[p + i] * 3 + 0]
+                        , vertices[triangles[p + i] * 3 + 1]
+                        , vertices[triangles[p + i] * 3 + 2]);
+                }
+                GL.Vertex3(vertices[triangles[p] * 3 + 0]
+                    , vertices[triangles[p] * 3 + 1]
+                    , vertices[triangles[p] * 3 + 2]);
+                GL.End();
+            }
+        }
+    }
+}
---
+++ util/u3d/U3DUtil.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Copyright (c) 2011 Stephen A. Pratt
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using UnityEngine;
+using Buffer = System.Buffer;
+using System.Collections.Generic;
+
+namespace org.critterai
+{
+    /// <summary>
+    /// Provides general purpose utility functions for Unity.
+    /// </summary>
+    public static class U3DUtil
+    {
+
+        /// <summary>
+        /// A string helpful in creating uniquely named project assets.
+        /// </summary>
+        /// <remarks>
+        /// <para>The value is only unique with the scope of the Unity 
+        /// project.</para>
+        /// <para>Format: objectName + n|p + objectId<br/>
+        /// Example: Agent-p3142</para>
+        /// </remarks>
+        /// <param name="unityObject">A Unity Object.</param>
+        /// <returns>A unique string for the object.</returns>
+        public static string GetCacheID(Object unityObject)
+        {
+            return unityObject.name 
+                + "-" + (unityObject.GetInstanceID() < 0 ? "n" : "p") 
+                + unityObject.GetInstanceID();
+        }
+
+        /// <summary>
+        /// Recursively searches the provided game objects for a type of
+        /// component.
+        /// </summary>
+        /// <typeparam name="T">The type of component to search for.</typeparam>
+        /// <param name="sources">An array of game objects to search.</param>
+        /// <returns>The components found during the search.</returns>
+        public static T[] GetComponents<T>(GameObject[] sources)
+            where T : Component
+        {
+            List<T> result = new List<T>();
+            foreach (GameObject go in sources)
+            {
+                if (go == null)
+                    continue;
+                T[] cs = go.GetComponentsInChildren<T>();
+                if (cs == null)
+                    continue;
+                foreach (T c in cs)
+                    result.Add(c);
+            }
+            return result.ToArray();
+        }
+    }
+}
