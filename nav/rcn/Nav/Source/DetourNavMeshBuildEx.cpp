add1ba2407c1863e618d1fc5fa6ecf4d220e67d2
stevefsp <stevefsp@67e97ef4-0a9b-11df-b317-0f3410dc57f6>
2011-07-23 17:54:35 +0000

    CAINav: Converted from <string> to <string.h> in order to support multi-OS builds. (See issue 13.)

--- nav/rcn/Nav/Source/DetourNavMeshBuildEx.cpp
+++ nav/rcn/Nav/Source/DetourNavMeshBuildEx.cpp
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#include <string>
+#include <string.h>
 #include "DetourNavMeshBuilder.h"
 #include "DetourCommon.h"
 #include "DetourNavMeshEx.h"
