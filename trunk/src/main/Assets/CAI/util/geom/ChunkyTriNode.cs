8417df21c2c0a345ba652c9e6cf77aca9d6f40f0
stevefsp <stevefsp@67e97ef4-0a9b-11df-b317-0f3410dc57f6>
2011-11-06 01:45:54 +0000

    cai-util: Added ChunkyTriMesh and moved Wavefront from u3d library to this library.

---
+++ trunk/src/main/Assets/CAI/util/geom/ChunkyTriNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace org.critterai.geom
+{
+    public struct ChunkyTriMeshNode
+    {
+        public float xmin;
+        public float zmin;
+        public float xmax;
+        public float zmax;
+        public int i;
+        public int count;
+    }
+}
