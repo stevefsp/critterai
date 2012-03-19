a8b527729051f214f4b410ef735ade190f356201
stevefsp <stevefsp@67e97ef4-0a9b-11df-b317-0f3410dc57f6>
2012-03-19 21:50:26 +0000

    CAINav: Branch builds into a .NET-only and Unity versions.
    Also updated assembly info for v0.4.

---
+++ build/assemblyInfo/cai-nav-u3d-editor.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Copyright (c) 2012 Stephen A. Pratt
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("cai-nav-u3d-editor")]
+[assembly: AssemblyDescription("Pathfinding and obstacle avoidance extensions for the Unity Editor.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("critterai.org")]
+[assembly: AssemblyProduct("CAINav")]
+[assembly: AssemblyCopyright("Copyright © Stephen Pratt 2011-2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
