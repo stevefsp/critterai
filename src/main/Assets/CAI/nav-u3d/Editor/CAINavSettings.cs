05c6140bf083a0218d6ff7e7ea776235f785a5e3
stevefsp <stevefsp@67e97ef4-0a9b-11df-b317-0f3410dc57f6>
2012-03-30 23:12:42 +0000

    CAINav: Added new source files related to v0.4.
    Unity: Various navigation asset and support elements. (Including the new navmesh assets.)
    Added NMGenProcessor and related Unity components to allow assignment of polygon flags based on area id.

---
+++ src/main/Assets/CAI/nav-u3d/Editor/CAINavSettings.cs
@@ -0,0 +1,175 @@
+﻿/*
+ * Copyright (c) 2012 Stephen A. Pratt
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using UnityEngine;
+using System.Collections.Generic;
+using org.critterai.nav;
+
+/// <summary>
+/// Global navigation related settings.
+/// </summary>
+[System.Serializable]
+public class CAINavSettings
+    : ScriptableObject
+{
+    private const string UnWalkable = "Not Walkable";
+    private const string Default = "Default";
+    private const string Unknown = "Unknown";
+
+    [SerializeField]
+    internal List<string> areaNames = new List<string>();
+
+    [SerializeField]
+    internal List<byte> areas = new List<byte>();
+
+    [SerializeField]
+    internal string[] flagNames;
+
+    void OnEnable()
+    {
+        if (areaNames.Count == 0)
+            Reset();
+    }
+
+    /// <summary>
+    /// Resets the settings to the default state.
+    /// </summary>
+    public void Reset()
+    {
+        areaNames.Clear();
+        areas.Clear();
+
+        areaNames.Add(UnWalkable);
+        areas.Add(Navmesh.UnwalkableArea);
+
+        areaNames.Add(Default);
+        areas.Add(Navmesh.MaxArea);
+
+        areaNames.Add("Water");
+        areas.Add((byte)(Navmesh.MaxArea - 1));
+
+        flagNames = new string[16];
+
+        flagNames[0] = Default;
+        flagNames[1] = "Jump";
+        flagNames[2] = "Swim";
+        flagNames[3] = "Blocked";
+
+        for (int i = 4; i < flagNames.Length; i++)
+        {
+            flagNames[i] = string.Format("Flag 0x{0:X}", 1 << i);
+        }
+    }
+
+    /// <summary>
+    /// Gets a clone of the defined well known area names.
+    /// </summary>
+    /// <returns>A clone of the area names.</returns>
+    public string[] GetAreaNames()
+    {
+        return areaNames.ToArray();
+    }
+
+    /// <summary>
+    /// True if the area has a defined well known name.
+    /// </summary>
+    /// <param name="area">The area.</param>
+    /// <returns>True if the area is well known.</returns>
+    public bool HasAreaName(byte area)
+    {
+        return (areas.IndexOf(area) >= 0);
+    }
+
+    /// <summary>
+    /// Gets the name of the area, or a default name if none has been defined.
+    /// </summary>
+    /// <param name="area">The area.</param>
+    /// <returns>The area's well known name.</returns>
+    public string GetAreaName(byte area)
+    {
+        int i = areas.IndexOf(area);
+        return (i == -1) ? Unknown : areaNames[i];
+    }
+
+    /// <summary>
+    /// Gets the index of the area's well known name. (Or -1 if it is not well known.)
+    /// </summary>
+    /// <param name="area">The area.</param>
+    /// <returns>The index of the well known name.</returns>
+    public int GetAreaNameIndex(byte area)
+    {
+        return areas.IndexOf(area);
+    }
+
+    /// <summary>
+    /// Gets the area associated with the well known name. (Or 0 if not a valid name.)
+    /// </summary>
+    /// <param name="name">The well known name.</param>
+    /// <returns>The area associated with the name.</returns>
+    public byte GetArea(string name)
+    {
+        int i = areaNames.IndexOf(name);
+        return (i == -1) ? (byte)0 : areas[i];
+    }
+
+    /// <summary>
+    /// Gets a clone of the well known names for flags, index on flag bit.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// All flags have a well known name.
+    /// </para>
+    /// <para>
+    /// The order of the names match the order of the flags. So the name at index 5 is the name
+    /// for flag (1 &lt;&lt; 5).
+    /// </para>
+    /// </remarks>
+    /// <returns>A clone of the flag names.</returns>
+    public string[] GetFlagNames()
+    {
+        return (string[])flagNames.Clone();
+    }
+
+    ///// <summary>
+    ///// Gets the well known name of the flag. (Only one should be set.)
+    ///// </summary>
+    ///// <param name="flag">The flag.</param>
+    ///// <returns></returns>
+    //public string GetFlagName(ushort flag)
+    //{
+    //    for (int i = 0; i < 16; i++)
+    //    {
+    //        if ((flag & (1 << i)) != 0)
+    //            return flagNames[i];
+    //    }
+    //    return "";
+    //}
+
+    //public int GetFlagNameIndex(ushort flag)
+    //{
+    //    for (int i = 0; i < 16; i++)
+    //    {
+    //        if ((flag & (1 << i)) != 0)
+    //            return i;
+    //    }
+    //    return -1;
+    //}
+}
---
+++ src/main/Assets/CAI/nav-u3d/Editor/CAINavSettingsEditor.cs
@@ -0,0 +1,177 @@
+/*
+ * Copyright (c) 2012 Stephen A. Pratt
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using org.critterai.nav;
+using org.critterai.u3d.editor;
+
+/// <summary>
+/// <see cref="CAINavSettings"/> editor.
+/// </summary>
+[CustomEditor(typeof(CAINavSettings))]
+public class CAINavSettingsEditor
+    : Editor
+{
+    private static bool mShowAreas = true;
+    private static bool mShowFlags = true;
+
+    private static string mNewName = "";
+    private static byte mNewArea = Navmesh.UnwalkableArea;
+
+    /// <summary>
+    /// Controls behavior of the inspector.
+    /// </summary>
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.Separator();
+
+        mShowAreas = EditorGUILayout.Foldout(mShowAreas, "Area Names");
+
+        if (mShowAreas)
+            OnGUIAreas();
+
+        EditorGUILayout.Separator();
+
+        mShowFlags = EditorGUILayout.Foldout(mShowFlags, "Flag Names");
+
+        if (mShowFlags)
+            OnGUIFlags();
+
+        EditorGUILayout.Separator();
+
+        if (GUI.changed)
+            EditorUtility.SetDirty(target);
+    }
+
+    private void OnGUIAreas()
+    {
+        CAINavSettings targ = (CAINavSettings)target;
+
+        List<byte> areas = targ.areas;
+        List<string> areaNames = targ.areaNames;
+
+        EditorGUILayout.Separator();
+
+        GUILayout.Label("Name / Area");
+
+        EditorGUILayout.Separator();
+
+        if (areas.Count > 0)
+        {
+            EditorGUILayout.BeginVertical();
+
+            int delChoice = -1;
+
+            for (int i = 0; i < areas.Count; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+
+                GUI.enabled = (areas[i] != Navmesh.UnwalkableArea);
+
+                string areaName = EditorGUILayout.TextField(areaNames[i]);
+
+                if (areaName == areaNames[i] || !areaNames.Contains(areaName))
+                    areaNames[i] = areaName;
+
+                GUI.enabled = !(areas[i] == Navmesh.UnwalkableArea || areas[i] == Navmesh.MaxArea);
+
+                byte area =
+                    NavUtil.ClampArea(EditorGUILayout.IntField(areas[i], GUILayout.Width(40)));
+
+                if (area == areas[i] || !areas.Contains(area))
+                    areas[i] = area;
+
+                if (GUILayout.Button("X"))
+                    delChoice = i;
+
+                GUI.enabled = true;
+
+                EditorGUILayout.EndHorizontal();
+            }
+
+            if (delChoice >= 0)
+            {
+                areaNames.RemoveAt(delChoice);
+                areas.RemoveAt(delChoice);
+                GUI.changed = true;
+            }
+
+            EditorGUILayout.EndVertical();
+        }
+
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.Separator();
+
+        EditorGUILayout.BeginHorizontal();
+
+        mNewName = EditorGUILayout.TextField(mNewName).Trim();
+        mNewArea =
+            NavUtil.ClampArea(EditorGUILayout.IntField(mNewArea, GUILayout.Width(40)));
+
+        GUI.enabled = (mNewName.Length > 0);
+
+        if (GUILayout.Button("Add"))
+        {
+            if (areaNames.Contains(mNewName) || areas.Contains(mNewArea))
+            {
+                Debug.LogError(string.Format("{0}: Name or area already defined: Name: {1}, Area: {2}"
+                    , targ.name, mNewName, mNewArea));
+            }
+            else
+            {
+                areaNames.Add(mNewName);
+                areas.Add(mNewArea);
+                mNewName = "";
+                mNewArea = Navmesh.UnwalkableArea;
+                GUI.changed = true;
+            }
+        }
+
+        GUI.enabled = true;
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndVertical();
+    }
+
+    private void OnGUIFlags()
+    {
+        CAINavSettings targ = (CAINavSettings)target;
+
+        string[] names = targ.flagNames;
+
+        EditorGUILayout.Separator();
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            names[i] = EditorGUILayout.TextField(string.Format("0x{0:X}", 1 << i), names[i]);
+        }
+    }
+
+    [MenuItem(EditorUtil.MainMenu + "Nav Settings", false, EditorUtil.GlobalGroup)]
+    static void EditSettings()
+    {
+        CAINavSettings item = EditorUtil.GetGlobalAsset<CAINavSettings>();
+        Selection.activeObject = item;
+    }
+}
